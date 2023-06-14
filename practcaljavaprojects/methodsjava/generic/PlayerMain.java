aa1dee8ab599ef5d039ed66463eea24e293398a6
JOSH <sibendirejoshua@gmail.com>
2023-06-14 21:33:29 +0300

    modified Team class and main method

--- practcaljavaprojects/methodsjava/generic/PlayerMain.java
+++ practcaljavaprojects/methodsjava/generic/PlayerMain.java
@@ -1,7 +1,20 @@
 package methodsjava.generic;
 
 public class PlayerMain {
+
+    /*
+    * Create objects for all classes
+    * */
     public static void main(String[] args) {
+ Football football = new Football("mesi");
+ Baseball baseball = new Baseball("moses");
+ Soccer soccer = new Soccer("mpape");
+
+ Team manu = new Team("manu");
+ manu.addPlayers(new Players("mesi"));
+ manu.addPlayers(new Players("moses"));
+ manu.addPlayers(new Players("mpape"));
+        System.out.println(manu.numPlayers());
 
     }
 }
--- practcaljavaprojects/methodsjava/generic/Team.java
+++ practcaljavaprojects/methodsjava/generic/Team.java
@@ -17,4 +17,43 @@ private ArrayList<Players> members = new ArrayList <>();
     public String getIdentity() {
         return identity;
     }
+    /*
+    This is a method that adds a player to the list
+    But you need to first check if the player is on the list
+     */
+    public boolean addPlayers(Players players){
+       if (members.contains(players)){
+           System.out.println(players.getIdentity() + "is already on the team");
+           return false;
+       }else {
+           members.add(players);
+           System.out.println(players.getIdentity() + " add to team" + this.identity);
+           return true;
+       }
+    }
+    /*
+    * use generic to check the list size of players
+    * */
+    public int numPlayers(){
+        return this.members.size();
+    }
+    /*
+    * Check the results
+    * */
+    public void matchResults(Team Opponent, int ourScore,int theirScore ){
+        if (ourScore > theirScore){
+            won++;
+        } else if (ourScore == theirScore) {
+           tied++;
+        }else {
+            lost++;
+        }
+        played++;
+        if (Opponent != null){
+            Opponent.matchResults(null,ourScore,theirScore);
+        }
+    }
+    public int ranking(){
+        return (won * 2) + tied;
+    }
 }
