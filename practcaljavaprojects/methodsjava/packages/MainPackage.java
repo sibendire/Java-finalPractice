e642e0720b26d6d96dffaa0b69329f7740e495a2
JOSH <sibendirejoshua@gmail.com>
2023-06-15 11:13:26 +0300

    modefied MyWindow class and MainPackage class

--- practcaljavaprojects/methodsjava/packages/MainPackage.java
+++ practcaljavaprojects/methodsjava/packages/MainPackage.java
@@ -5,8 +5,9 @@ import org.w3c.dom.Node;
 public class MainPackage {
     public static void main(String[] args) {
 
-     Node node= null;
-  node.getTextContent();
-
+//        Node node = null;
+//        node.getTextContent();
+     MyWindow myWindow = new MyWindow("By joshua");
+     myWindow.setVisible(true);
     }
 }
--- practcaljavaprojects/methodsjava/packages/MyWindow.java
+++ practcaljavaprojects/methodsjava/packages/MyWindow.java
@@ -1,5 +1,30 @@
 package methodsjava.packages;
 
-public class MyWindow {
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+public class MyWindow extends Frame {
+
+    public MyWindow(String title) {
+        super(title);
+        setSize(120,730);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sarsSerifLarge = new Font("sarsSerif",Font.BOLD,18);
+        Font sarsSerifSmall = new Font("sarsSerifSmall",Font.ITALIC,12);
+        g.setFont(sarsSerifLarge);
+        g.drawString("complete master class course",60,60);
+        g.setFont(sarsSerifSmall);
+        g.drawString("Your open to work",60,100 );
+    }
 }
